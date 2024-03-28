677f390daaf60ec085fbcd6903d645d1cba67cc2
d33pster <d33pster@icloud.com>
2024-03-28 12:41:20 +0530

    improved argument handling

--- csv2libsvm.py
+++ csv2libsvm.py
@@ -10,6 +10,65 @@ Expecting no headers. If present, headers can be skipped with argv[4] == 1.
 import sys
 import csv
 from collections import defaultdict
+from optioner import options
+
+
+def helper():
+    print('csv2libsvm. by zygmuntz. edited by d33pster')
+    print('\nHELP TEXT\n')
+    print('  |     -h or --help          : show this help text and exit.')
+    print('     COMPULSORY ARGUMENTS:')
+    print('  |     -i or --infile        : specify input file.')
+    print('  |     -o or --outfile       : specify output file.')
+    print('     OPTIONAL ARGUMENTS:')
+    print('  |     -li or --label-index  : specify label index. if no labels in input file, set to -1.')
+    print('  |     -sh or --skip-headers : takes 0 or 1. if there are headers in input file, set to 1.')
+    print('END')
+    exit(0)
+
+# create arguments
+shortargs = ['i', 'o', 'li', 'sh', 'h']
+longargs = ['infile', 'outfile', 'label-index', 'skip-headers', 'help']
+compulsory_short_args = ['i', 'o']
+compulsory_long_args = ['infile', 'outfile']
+
+argCTRL = options(shortargs, longargs, sys.argv[1:], compulsory_short_args, compulsory_long_args, ['-h', '--help'])
+
+actualargs, check, error, falseargs = argCTRL._argparse()
+
+if not check:
+    print(f'ERROR: {error}')
+    exit(1)
+else:
+    if '-h' in actualargs or '--help' in actualargs:
+        helper()
+    
+    # check optional args first
+    if '-li' in actualargs:
+        label_index = argCTRL._what_is_('li')
+    elif '--label-index' in actualargs:
+        label_index = argCTRL._what_is_('label-index')
+    else:
+        pass
+    
+    if '-sh' in actualargs:
+        skip_headers = argCTRL._what_is_('sh')
+    elif '--skip-headers' in actualargs:
+        skip_headers = argCTRL._what_is_('skip-headers')
+    else:
+        pass
+    
+    # compulsory args
+    if '-i' in actualargs:
+        input_file = argCTRL._what_is_('i')
+    else:
+        input_file = argCTRL._what_is_('infile')
+    
+    if '-o' in actualargs:
+        output_file = argCTRL._what_is_('o')
+    else:
+        output_file = argCTRL._what_is_('outfile')
+    
 
 def construct_line( label, line ):
 	new_line = []
@@ -28,18 +87,18 @@ def construct_line( label, line ):
 
 # ---
 
-input_file = sys.argv[1]
-output_file = sys.argv[2]
+# input_file = sys.argv[1]
+# output_file = sys.argv[2]
 
-try:
-	label_index = int( sys.argv[3] )
-except IndexError:
-	label_index = 0
+# try:
+# 	label_index = int( sys.argv[3] )
+# except IndexError:
+# 	label_index = 0
 
-try:
-	skip_headers = sys.argv[4]
-except IndexError:
-	skip_headers = 0
+# try:
+# 	skip_headers = sys.argv[4]
+# except IndexError:
+# 	skip_headers = 0
 
 i = open( input_file, 'rb' )
 o = open( output_file, 'wb' )
